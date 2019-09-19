a32ee9121679f4230b89950f58207d43dc5b30f3
Serhii Rodik <serhiirodik@gmail.com>
2019-09-19 18:50:44 +0200

    Массив

--- js/main.js
+++ js/main.js
@@ -1,30 +1,10 @@
-
 var photoDescription = [];
 
 var demoDataArray = function() {
   for (i = 0; photoDescription.length < 25; photoDescription.length++) {
-      // console.log(photoDescription.length);
+      console.log(photoDescription.length);
   }
- return photoDescription = demoDataArray();
+
 }
 
-
-
-console.log(photoDescription = demoDataArray());
-
-
-
-
-
-
-
-
-
-
-// var photoDescription = {
-//     url: 1,
-//     description: 2,
-//     likes: 3,
-//     comments: 4
-//   }
-
+demoDataArray();
