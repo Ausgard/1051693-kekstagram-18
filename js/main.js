3496b9208f3c40e9e97e461840e51638255eb7be
Serhii Rodik <serhiirodik@gmail.com>
2019-09-23 09:18:15 +0200

    Заполнение блока DOM-элементами на основе массива JS-объектов

--- js/main.js
+++ js/main.js
@@ -56,87 +56,50 @@ var createDemoDataArray = function (length) {
   }
   return array;
 }
-var photos = createDemoDataArray(5);
+var photos = createDemoDataArray(25);
 
 // Создание DOM-элемента на основе JS-объекта
 var createPhotoNode = function(element) {
-
-var pictureLink = function() {
-
-var createPictureLink = document.createElement('a');
-var createPictureImg = document.createElement('img');
-var createPictureInfo = document.createElement('p');
-var createPictureLikes = document.createElement('span');
-var createPictureComments = document.createElement('span');
-
-var childImg;
-var childInfo;
-var childComments;
-var childLikes;
-
-var photoBlock;
-
-for (var i = 0; i < photos.length; i++) {
-
-childImg = createPictureLink.appendChild(createPictureImg);
-childInfo = createPictureLink.appendChild(createPictureInfo);
-childLikes = childInfo.appendChild(createPictureLikes);
-
-childComments = childInfo.appendChild(createPictureComments);
-
-createPictureLink.setAttribute('href', '#');
-createPictureImg.setAttribute('src', 'photos/' + getRandomNumber(1, 25) + '.jpg');
-createPictureImg.setAttribute('width', '182');
-createPictureImg.setAttribute('height', '182');
-createPictureImg.setAttribute('alt', 'Случайная фотография');
-
-createPictureLink.classList.add("picture");
-createPictureImg.classList.add("picture__img");
-createPictureInfo.classList.add("picture__info");
-createPictureComments.classList.add("picture__comments");
-createPictureLikes.classList.add("picture__likes");
-
-createPictureLikes.innerHTML = getRandomNumber(15, 200);
-createPictureComments.innerHTML = comments[getRandomNumber(0, 7)];
-
-
-photoBlock = createPictureLink;
-
+  var createPictureLink = document.createElement('a');
+  var createPictureImg = document.createElement('img');
+  var createPictureInfo = document.createElement('p');
+  var createPictureLikes = document.createElement('span');
+  var createPictureComments = document.createElement('span');
+  var childImg;
+  var childInfo;
+  var childComments;
+  var childLikes;
+  var photoBlock;
+  for (var i = 0; i < photos.length; i++) {
+    childImg = createPictureLink.appendChild(createPictureImg);
+    childInfo = createPictureLink.appendChild(createPictureInfo);
+    childLikes = childInfo.appendChild(createPictureLikes);
+    childComments = childInfo.appendChild(createPictureComments);
+    createPictureLink.setAttribute('href', '#');
+    createPictureImg.setAttribute('src', 'photos/' + getRandomNumber(1, 25) + '.jpg');
+    createPictureImg.setAttribute('width', '182');
+    createPictureImg.setAttribute('height', '182');
+    createPictureImg.setAttribute('alt', 'Случайная фотография');
+    createPictureLink.classList.add("picture");
+    createPictureImg.classList.add("picture__img");
+    createPictureInfo.classList.add("picture__info");
+    createPictureComments.classList.add("picture__comments");
+    createPictureLikes.classList.add("picture__likes");
+    createPictureLikes.innerHTML = getRandomNumber(15, 200);
+    createPictureComments.innerHTML = comments[getRandomNumber(0, 7)];
+    photoBlock = createPictureLink;
+  }
+  return photoBlock;
 }
-
-return photoBlock;
-
-} //вкладена ф-я
-
-for (var i = 0; i < photos.length; i++) {
-   pictureLink(photos[i]);
-   console.log(pictureLink());
-}
-
-var pictureLinkResult = pictureLink();
-
-return pictureLinkResult;
-
-} //Основна ф-я
-
-// var photoBlock = createPhotoNode();
-
-console.log(createPhotoNode());
 
 // Заполнение блока DOM-элементами на основе массива JS-объектов
-
-var insertPhotoBlock = function() {
-
-var test = document.querySelector('#picture');
-var fragment = document.createDocumentFragment();
-
-for (var i = 0; i < photos.length; i++) {
-  test.append(photoBlock);
-  console.log(photoBlock);
+var insertPhotoNode = function() {
+  var block = document.querySelector('#picture');
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < photos.length; i++) {
+    block.append(createPhotoNode());
+  }
+  return block;
 }
 
-return test;
-
-}
-
-console.log(insertPhotoBlock());
+insertPhotoNode();
