60b1a9aa250f44024fae63d62ca7e15ddae12f59
Serhii Rodik <serhiirodik@gmail.com>
2019-09-26 19:50:13 +0200

    test

--- js/main.js
+++ js/main.js
@@ -91,3 +91,32 @@ for (var i = 0; i < photos.length; i++) {
   fragment.appendChild(createPhotoNode(photos[i]));
 }
 block.appendChild(fragment);
+
+// Первая фотография из массива объектов в полноразмерном режиме
+
+var createBigPhotoNode = function (photo) {
+
+var bigPicture = document.querySelector('.big-picture');
+bigPicture.classList.remove('hidden');
+
+var bigPictureImg = document.querySelector('.big-picture__img img:nth-of-type(1)');
+bigPictureImg.setAttribute('src', photo.url);
+
+var bigPicturelikes = document.querySelector('.likes-count')
+bigPicturelikes.innerHTML = photo.likes;
+
+var bigPictureCommentsCount = document.querySelector('.comments-count');
+bigPictureCommentsCount.innerHTML = photo.comments.length;
+
+var bigPictureCommentsImg = document.querySelector('.social__picture');
+bigPictureCommentsImg.setAttribute('src', 'img/avatar-' + getRandomNumber(1, 6) + '.svg');
+bigPictureCommentsImg.setAttribute('alt', authors[getRandomNumber(0, 12)]);
+
+var bigPictureCommentsText = document.querySelector('.social__comment p:nth-of-type(1)');
+bigPictureCommentsText.innerHTML = comments[getRandomNumber(0, 7)];
+
+return bigPicture;
+}
+
+console.log(createBigPhotoNode(photos[0]));
+
