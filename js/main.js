3f25dacfbf4a369c26d5a02d8ab21ca464d2f8e3
Serhii Rodik <serhiirodik@gmail.com>
2019-09-26 19:57:28 +0200

    Создание большой фотографии из массива объектов в полноразмерном режиме

--- js/main.js
+++ js/main.js
@@ -93,30 +93,20 @@ for (var i = 0; i < photos.length; i++) {
 block.appendChild(fragment);
 
 // Создание большой фотографии из массива объектов в полноразмерном режиме
-
 var createBigPhotoNode = function (photo) {
-
-var bigPicture = document.querySelector('.big-picture');
-bigPicture.classList.remove('hidden');
-
-var bigPictureImg = document.querySelector('.big-picture__img img:nth-of-type(1)');
-bigPictureImg.setAttribute('src', photo.url);
-
-var bigPicturelikes = document.querySelector('.likes-count')
-bigPicturelikes.innerHTML = photo.likes;
-
-var bigPictureCommentsCount = document.querySelector('.comments-count');
-bigPictureCommentsCount.innerHTML = photo.comments.length;
-
-var bigPictureCommentsImg = document.querySelector('.social__picture');
-bigPictureCommentsImg.setAttribute('src', 'img/avatar-' + getRandomNumber(1, 6) + '.svg');
-bigPictureCommentsImg.setAttribute('alt', authors[getRandomNumber(0, 12)]);
-
-var bigPictureCommentsText = document.querySelector('.social__comment p:nth-of-type(1)');
-bigPictureCommentsText.innerHTML = comments[getRandomNumber(0, 7)];
-
-return bigPicture;
-}
-
-console.log(createBigPhotoNode(photos[0]));
-
+  var bigPicture = document.querySelector('.big-picture');
+  bigPicture.classList.remove('hidden');
+  var bigPictureImg = document.querySelector('.big-picture__img img:nth-of-type(1)');
+  bigPictureImg.setAttribute('src', photo.url);
+  var bigPicturelikes = document.querySelector('.likes-count');
+  bigPicturelikes.innerHTML = photo.likes;
+  var bigPictureCommentsCount = document.querySelector('.comments-count');
+  bigPictureCommentsCount.innerHTML = photo.comments.length;
+  var bigPictureCommentsImg = document.querySelector('.social__picture');
+  bigPictureCommentsImg.setAttribute('src', 'img/avatar-' + getRandomNumber(1, 6) + '.svg');
+  bigPictureCommentsImg.setAttribute('alt', authors[getRandomNumber(0, 12)]);
+  var bigPictureCommentsText = document.querySelector('.social__comment p:nth-of-type(1)');
+  bigPictureCommentsText.innerHTML = comments[getRandomNumber(0, 7)];
+  return bigPicture;
+};
+createBigPhotoNode(photos[0]);
