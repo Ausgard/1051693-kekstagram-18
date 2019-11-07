3de20afa50818a8b261815ae645d38dded6a6b70
Ausgard <serhiirodik@gmail.com>
2019-11-07 09:09:15 +0200

    Закрытие

--- js/main.js
+++ js/main.js
@@ -249,11 +249,27 @@ var uploadedImgBlockCancel = document.querySelector('.img-upload__cancel');
 var ploadFormButton = document.querySelector('.img-upload__submit');
 var hashtagInput = document.querySelector('.text__hashtags');
 
+function closePreviewPhotoButton(event) {
+  var hashtagInputActive;
+  if (document.activeElement === hashtagInput) {
+    hashtagInputActive = true;
+  } else {
+    hashtagInputActive = false;
+  }
+  if (event.keyCode === 27) {
+    if (!uploadedImgBlock.classList.contains('hidden') || !hashtagInputActive) {
+      setDefaultFilterValue();
+      uploadedImgBlock.classList.add('hidden');
+    }
+  }
+}
+
 function closePreviewPhotoMouse() {
   uploadedImgBlock.classList.add('hidden');
   setDefaultFilterValue();
 }
 uploadedImgBlockCancel.addEventListener('click', closePreviewPhotoMouse);
+hashtagInput.addEventListener('keydown', closePreviewPhotoButton);
 
 function setDefaultFilterValue() {
   prewiewImg.style.transform = 'scale(' + zoomValue + ')';
@@ -444,7 +460,7 @@ ploadFormButton.addEventListener('click', createComentString);
 
 function closeComentTextarea(event) {
   var closeFlag;
-  if (document.activeElement === hashtagInput || document.activeElement === comentInput) {
+  if (document.activeElement === comentInput) {
     closeFlag = true;
   } else {
     closeFlag = false;
